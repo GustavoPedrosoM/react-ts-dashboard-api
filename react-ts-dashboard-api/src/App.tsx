2d29f07b71f56c6f4caceb510451897f5711a4f0
GustavoPedrosoM <gustavo2002machado@hotmail.com>
2025-11-05 18:37:23 -0300

    Renderizando o dashboard em app.tsx

--- react-ts-dashboard-api/src/App.tsx
+++ react-ts-dashboard-api/src/App.tsx
@@ -1,11 +1,11 @@
+import { Dashboard } from "./pages/Dashboard";
+
 function App() {
   return (
-    <div className="w-screen h-screen bg-slate-900 flex justify-center p-6">
-      <h1 className="text-4xl font-bold text-white">
-        Dashboard de Dados 
-      </h1>
+    <div className="min-h-screen bg-gray-100 text-gray-800">
+      <Dashboard />
     </div>
-  )
+  );
 }
 
-export default App 
+export default App;
